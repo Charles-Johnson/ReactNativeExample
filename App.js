c0d04d569fab0c6ac0189dc5fc4ceab0cf46d585
Charles Johnson <charlesthomasjohnson0@gmail.com>
2019-01-29 17:04:36 +0900

    Section titles are now buttons for a drop menu

--- App.js
+++ App.js
@@ -1,26 +1,52 @@
 import React, {Component} from 'react';
-import {SectionList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {Alert, SectionList, TouchableHighlight, ScrollView, Button, StyleSheet, Text, View, ViewToken } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+// Titles of each section of the menu
+const titles = ['첫번째 선택', '둘번째 선택', '셋번째 선택', '넷번째 선택'];
+
+export default class Menu extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {section_is_shown: {}};
+		// Initially all sections are collapsed
+		for (title of titles) {
+			this.state.section_is_shown[title] = false;
+		}
+	}
   render() {
     return (
       <ScrollView contentContainerStyle={styles.container}>
-				<SectionList 
+				<SectionList
+					// Information for each section
 		  		sections={[
-						{title: '첫번째 선택', data: ['가', '나', '다']},
-						{title: '둘번째 선택', data: ['가', '나']},
-						{title: '셋번째 선택', data: ['가', '나', '다', '라', '가', '나', '다', '라']},
-						{title: '넷번째 선택', data: ['가']},
+						{title: titles[0], data: ['가', '나', '다']},
+						{title: titles[1], data: ['가', '나']},
+						{title: titles[2], data: ['가', '나', '다', '라', '마', '바', '사', '아']},
+						{title: titles[3], data: ['가']},
 					]}
-					renderItem={({item, index, section}) =>
-							<Text style={styles.text}>{item}</Text>
-		  		}
-					renderSectionHeader={({section: {title}}) => (
-						<View style={styles.option}> 
-							<Text style={styles.text}>{title}</Text>
-						</View>
-					)}
-  				keyExtractor={(item, index) => item + index}
+					// Renders the information for each section depending on the state
+					renderItem={
+						({item, index, section: {title}}) =>
+							(this.state.section_is_shown[title]) ? 
+								<Text style={styles.text}>{item}</Text> : null
+					}
+					// Renders the section titles as buttons which uncollapse or collapse the section information depending on the state.
+					renderSectionHeader={
+						({section: {title}}) => {
+							let new_section_is_shown = Object.assign({}, this.state.section_is_shown);
+							new_section_is_shown[title] = !new_section_is_shown[title];
+							return (<TouchableHighlight onPress={
+								() => this.setState(
+									{section_is_shown: new_section_is_shown}
+								)
+							}>
+								<View style={styles.option}> 
+									<Text style={styles.text}>{title}</Text>
+								</View>
+							</TouchableHighlight>);
+						}
+					}
+  				keyExtractor={(item, index) => item + index} // A warning is given without this prop 
 				/>
       </ScrollView>
     );
